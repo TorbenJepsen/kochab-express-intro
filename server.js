cbac018d92900d4733fcd55ac235f50294c318ba
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-17 11:52:37 -0500

    add get requests

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+// req is request res is response
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`);
+});
+
+app.get('/hey', function (req, res) {
+    res.send('Howdy!');
+});
